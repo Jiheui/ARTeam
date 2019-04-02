0ac3e22e34d3704d04f12d5e2bbbd20a914666af
Norton-Xu <www.xpl1223hhh@gmail.com>
2019-04-03 00:36:20 +1100

    Added "scan poster" and "back to main scene" buttons

---
+++ ARPosterReader/Assets/BackToMain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // This is called when the "back" button is clicked, 
+    // then the main scene comes back
+    // Wrote by Norton
+    public void BackToMainScene(string scenename) {
+        SceneManager.LoadScene(scenename);
+    }
+}
