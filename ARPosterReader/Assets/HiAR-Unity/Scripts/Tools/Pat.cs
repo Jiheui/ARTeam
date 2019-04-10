03dcc850396d69e9cc12f843c022eae3e92a6bb9
Yutao GE <chris.dfo.only@gmail.com>
2019-04-10 23:08:18 +1000

    Update RESTful API
    upload some basic code

---
+++ ARPosterReader/Assets/HiAR-Unity/Scripts/Tools/Pat.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using System.Net;
+using System.IO;
+using Proyecto26;
+
+public class NewBehaviourScript {
+	public void ff() {
+	}
+}
