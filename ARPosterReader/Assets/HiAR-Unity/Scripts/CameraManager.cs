bfdcae35f3dfcd94c4750ac52dc7a342dc3ecfd7
Lachlan Grainger <u5847571@anu.edu.au>
2019-04-10 16:28:55 +1000

    Progress in creating a zoom screen
    Have added another camera, and some scripts but still lacks functionality

---
+++ ARPosterReader/Assets/HiAR-Unity/Scripts/CameraManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CameraState{
+	HiAR,
+	Zoom
+}
+
+public class CameraManager : MonoBehaviour {
+
+	public GameObject HiARCamera;
+	public GameObject zoomCamera;
+
+	private CameraState cameraState = CameraState.HiAR;
+
+	private AudioListener HiARAudioListener;
+	private AudioListener zoomAudioListener;
+
+	void Start () {
+		HiARAudioListener = HiARCamera.GetComponent<AudioListener>();
+		zoomAudioListener = zoomCamera.GetComponent<AudioListener>();
+
+		SetCameras(cameraState);
+	}
+
+	new void SendMessage(string str) {
+		cameraState = (cameraState == CameraState.HiAR) ? CameraState.Zoom : CameraState.HiAR;
+		SetCameras(cameraState);
+	}
+
+	void SetCameras(CameraState state){
+		bool isHiAR = state == CameraState.HiAR;
+
+		HiARCamera.SetActive(isHiAR);
+		HiARAudioListener.enabled = isHiAR;
+
+		zoomCamera.SetActive(!isHiAR);
+		zoomAudioListener.enabled = !isHiAR;
+	}
+}
