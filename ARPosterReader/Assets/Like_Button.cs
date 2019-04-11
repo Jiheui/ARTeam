b7f93204f3acef55b0a6661d84bb114014c7919b
Yue Jiang <u6311789@anu.edu.au>
2019-04-11 18:37:54 +1000

    favourite page done partly
    1.The thumbnail and title can be viewed when entering the favourite page
    2. The red heart button can be cliced and then it will change to the white heart

---
+++ ARPosterReader/Assets/Like_Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Like_Button : MonoBehaviour {
+
+	public Sprite red_heart;
+	public Sprite white_heart;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void unlikePoster(){
+		gameObject.GetComponent<Image>().sprite = white_heart;
+	}
+}
