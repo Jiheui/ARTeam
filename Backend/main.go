4f94d5f3206cb4c22e04316216f03650671745cc
Yutao GE <chris.dfo.only@gmail.com>
2019-04-12 21:46:26 +1000

    remove unused import

--- Backend/main.go
+++ Backend/main.go
@@ -3,14 +3,13 @@
 * @E-mail: u6283016@anu.edu.au
 * @Date:   2019-03-28 18:52:31
 * @Last Modified by:   Yutao Ge
-* @Last Modified time: 2019-04-12 02:24:32
+* @Last Modified time: 2019-04-12 21:45:50
  */
 package main
 
 import (
 	"log"
 	"net/http"
-	"os"
 
 	model "./Models"
 
