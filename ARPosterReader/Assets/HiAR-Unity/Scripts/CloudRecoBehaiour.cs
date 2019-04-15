e6ea54eb91e4a294a90cdc28b3a255e4f7b39789
jkbob2 <635560243@qq.com>
2019-04-15 19:32:00 +1000

    Add the Cloud Recognize into Test Scene
    Try to use the Cloud Recognition into Test Scene

--- ARPosterReader/Assets/HiAR-Unity/Scripts/CloudRecoBehaiour.cs
+++ ARPosterReader/Assets/HiAR-Unity/Scripts/CloudRecoBehaiour.cs
@@ -1,14 +1,22 @@
 using System;
 using hiscene;
+using Models;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CloudRecoBehaiour : CloudRecognition,ICloudRecoEventHandler
 {
+    Text timeText;
+    Text addressText;
+    Text linkText;
 
     new void Start()
     {
         base.Start();
         RegisterCloudRecoEventHandler(this);
+        timeText = GameObject.Find("Time").GetComponent<Text>();
+        addressText = GameObject.Find("Address").GetComponent<Text>();
+        linkText = GameObject.Find("Web Link").GetComponent<Text>();
     }
 
 	public override void OnCloudReco(RecoResult recoResult){
@@ -20,6 +28,22 @@ public class CloudRecoBehaiour : CloudRecognition,ICloudRecoEventHandler
         {
             bindingGameObject(gameObject, recoResult.KeyId);
         }
+
+        Poster poster = new Poster();
+        poster.keygroup = recoResult.KeyGroup;
+        poster.keyid = recoResult.KeyId;
+        poster.GetPoster();
+        showDetail(poster.detail);
+    }
+
+    public void showDetail(string detail)
+    {
+        string detailRaw = detail;
+        string[] detailList = detailRaw.Split(';');
+
+        timeText.text = detailList[0];
+        addressText.text = detailList[1];
+        linkText.text = detailList[2];
     }
 
     public override GameObject createGameObject(RecoResult recoResult)
