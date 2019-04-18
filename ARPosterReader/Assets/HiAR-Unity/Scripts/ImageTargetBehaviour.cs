6dcde0d273631a64300023d017987ae26ebb2665
Norton-Xu <www.xpl1223hhh@gmail.com>
2019-04-18 21:59:29 +1000

    Added code for #100.5.1

--- ARPosterReader/Assets/HiAR-Unity/Scripts/ImageTargetBehaviour.cs
+++ ARPosterReader/Assets/HiAR-Unity/Scripts/ImageTargetBehaviour.cs
@@ -99,6 +99,36 @@ public class ImageTargetBehaviour : ImageTarget, ITrackableEventHandler, ILoadBu
         linkText.text = "Web Link";
     }
 
+    public void OpenURLOnClick(string urlToOpen)
+    {
+        bool fail = false;
+        AndroidJavaClass up = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ca = up.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject packageManager = ca.Call<AndroidJavaObject>("getPackageManager");
+
+        AndroidJavaObject launchIntent = null;
+        try
+        {
+            launchIntent = packageManager.Call<AndroidJavaObject>("getLaunchIntentForPackage", urlToOpen);
+        }
+        catch (System.Exception e)
+        {
+            fail = true;
+        }
+
+        if (fail)
+        { //open app in store
+            Application.OpenURL("https://google.com");
+        }
+        else //open the app
+            ca.Call("startActivity", launchIntent);
+
+        up.Dispose();
+        ca.Dispose();
+        packageManager.Dispose();
+        launchIntent.Dispose();
+    }
+
     public virtual void OnTargetTracked(RecoResult recoResult, Matrix4x4 pose) { }
 
     public virtual void OnTargetLost(RecoResult recoResult)
