812c1873df9813c775f6996383c7659c894658e5
danielchen <u6293777@anu.edu.au>
2019-04-24 19:11:24 +1000

    Update Login Page
    Update Login Page

---
+++ ARPosterReader/Assets/SelfLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SelfLogin : MonoBehaviour {
+
+	public InputField emial;
+	public InputField password;
+
+	public void getTextLoginAndRegister()
+	{
+		
+	}
+		
+}
