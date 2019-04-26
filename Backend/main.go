452e29bb6f6ab486de0365e40cdcc9f823e90993
Yutao GE <chris.dfo.only@gmail.com>
2019-04-27 00:34:43 +1000

    Fix upload/download file

--- Backend/main.go
+++ Backend/main.go
@@ -3,7 +3,7 @@
 * @E-mail: u6283016@anu.edu.au
 * @Date:   2019-03-28 18:52:31
 * @Last Modified by:   Yutao Ge
-* @Last Modified time: 2019-04-22 09:36:02
+* @Last Modified time: 2019-04-27 00:21:56
  */
 package main
 
@@ -21,10 +21,12 @@ func main() {
 	p := model.PosterResource{}
 	f := model.FavouritePosterResource{}
 	fb := model.FeedbackResource{}
+	file := model.FileResource{}
 	restful.DefaultContainer.Add(u.WebService())
 	restful.DefaultContainer.Add(p.WebService())
 	restful.DefaultContainer.Add(f.WebService())
 	restful.DefaultContainer.Add(fb.WebService())
+	restful.DefaultContainer.Add(file.WebService())
 
 	log.Printf("start listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
