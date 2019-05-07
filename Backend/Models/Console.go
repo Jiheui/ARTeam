102e7997c9847906f50a38cac0b6ec2010c14cb4
Yutao GE <chris.dfo.only@gmail.com>
2019-05-07 22:44:19 +1000

    update console login UI

--- Backend/Models/Console.go
+++ Backend/Models/Console.go
@@ -3,20 +3,20 @@
 * @E-mail: u6283016@anu.edu.au
 * @Date:   2019-05-06 22:43:42
 * @Last Modified by:   Yutao Ge
-* @Last Modified time: 2019-05-07 00:12:10
+* @Last Modified time: 2019-05-07 22:43:55
  */
 package Models
 
 import (
-	"log"
 	//"net/http"
 	"text/template"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/emicklei/go-restful"
 )
 
 type Console struct {
-	Text string
+	StaticFilePrefix string
 }
 
 type ConsoleResource struct {
@@ -30,14 +30,17 @@ func (c *ConsoleResource) WebService() *restful.WebService {
 		Produces(restful.MIME_OCTET, restful.MIME_XML, restful.MIME_JSON)
 
 	ws.Route(ws.GET("/").To(c.Index))
+	ws.Route(ws.GET("/login").To(c.Index))
 
 	return ws
 }
 
+// Login page
 func (c *ConsoleResource) Index(request *restful.Request, response *restful.Response) {
+	log.Info(request.Request.Host)
 	p := &Console{"restful-html-template demo"}
 	// you might want to cache compiled templates
-	t, err := template.ParseFiles("Models/Templates/console.html")
+	t, err := template.ParseFiles("Models/Templates/login.html")
 	if err != nil {
 		log.Fatalf("Template gave: %s", err)
 	}
