197adc10c5cdf55d4c2cc5c168a0a4ae6af2bc39
Yue Jiang <u6311789@anu.edu.au>
2019-05-09 13:38:21 +1000

    Improvement of login and logout
    1. Add a local file to save login user's info
    2. user don't need to login again unless user logout

---
+++ ARPosterReader/Assets/Check_Login.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class Check_Login : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		string path = "Assets/HiAR-Unity/Resources/User_Info/User.txt";
+
+		//Read the text from directly from the test.txt file
+		StreamReader reader = new StreamReader(path); 
+		string id = reader.ReadLine ();
+//		print (id);
+		reader.Close();
+
+		if (id != null) {
+			storeLoginSessionId.loginId = int.Parse (id);
+			SceneManager.LoadScene("HiARRobot");
+//			print (storeLoginSessionId.loginId + " good");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
