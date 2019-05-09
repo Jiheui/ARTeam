e846d14a10a672258b07ef6654984d7a7470fa8a
ZEMIN/CHEN <u6293777@anu.edu.au>
2019-05-09 21:22:08 +1000

    Feedback Function
    Feedback Function without connected to the database

--- ARPosterReader/Assets/Feedback.cs
+++ ARPosterReader/Assets/Feedback.cs
@@ -1,8 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Models;
 
 public class Feedback : MonoBehaviour {
+	public InputField _feedback;
+
+
+
+
+	public void AddtoDatabase()
+	{
+//		User u = new User ();
+//		string email = u.email;
+//		int id = u.id;
+//		string username = u.username;
+
+		string f = _feedback.text;
+		Debug.Log ("hello world" + f);
+
+
+
+		// u.SendFeedback();
+	}
 
 	// Use this for initialization
 	void Start () {
