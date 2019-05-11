a413fdedb586a7a1221f87b667addc3419d5c418
jiheui <jiheui1997@gmail.com>
2019-05-11 16:10:14 +1000

    Ray Casting for 3D object and UI interaction
    Also changed a Active full poster to active ray cast

---
+++ ARPosterReader/Assets/ActiveRayCast.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+/**this script is used for "Raycasting, recognizing whether an object has been selected by the user or not", 
+    
+
+    Author: Ji Heui Yu
+    Date: 20/04/2019
+**/
+public class ActiveRayCast : MonoBehaviour
+{
+    public Camera c;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton (0)){
+            RaycastHit hit;
+            Ray ray = c.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,out hit,100.0f)){
+                Transform objectHit = hit.transform;
+
+                objectHit.Translate(100, 0, 0);
+            }
+        }
+
+    }
+}
