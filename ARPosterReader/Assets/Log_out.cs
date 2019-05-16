0546ee00116f45643945fe8060bafac23bf52fed
Yue Jiang <u6311789@anu.edu.au>
2019-05-16 11:18:27 +1000

    improvement on code
    change some codes to encapsulate

--- ARPosterReader/Assets/Log_out.cs
+++ ARPosterReader/Assets/Log_out.cs
@@ -8,26 +8,21 @@ using System.IO;
 
 public class Log_out : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	public void logOut(string nextScene){
 		if (storeLoginSessionId.loginId != -1) {
 			storeLoginSessionId.loginId = -1;
 
-            string path = Application.persistentDataPath + "/User.txt";
-            StreamWriter clearFile = File.CreateText (path);
-			clearFile.Flush ();
-			clearFile.Close ();
+			clearLocalFile ();
 
 			SceneManager.LoadScene(nextScene);
 		}
+	}
+
+	public void clearLocalFile (){
+		string path = Application.persistentDataPath + "/User.txt";
+		StreamWriter clearFile = File.CreateText (path);
+		clearFile.Flush ();
+		clearFile.Close ();
 	}
 }
