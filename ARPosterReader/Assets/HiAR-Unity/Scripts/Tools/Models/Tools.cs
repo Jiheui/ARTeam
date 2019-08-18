61a3f06a53694d1ae56770d45ca553fafccf0f2c
jkbob2 <635560243@qq.com>
2019-08-18 15:35:22 +1000

    Remove the old project
    Remove the old project

--- ARPosterReader/Assets/HiAR-Unity/Scripts/Tools/Models/Tools.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-public class Tools
-{
-	public static string serverIP {get { return "ar.shmily.me";}}
-	public static string serverPort {get { return "8080";}}
-
-	public string Server { get { return serverIP + ":" + serverPort;}}
-}
