10afc436e1cd7f364fe0c3ceb721f6e44ea74177
ZEMIN/CHEN <u6293777@anu.edu.au>
2019-09-04 21:42:46 +1000

    Personalize Posters
    Now can personalize posters in three-D model

---
+++ ARPosterReader/Assets/personalizePoster3D.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Models;
+using UnityEngine;
+
+public class personalizePoster3D : MonoBehaviour
+{
+    public TextMesh textMesh; 
+
+    public void Start()
+    {
+        User u = new User();
+        u.id = storeLoginSessionId.loginId;
+
+        u.FindUser();
+        if (u.authenticated)
+        {
+            if (string.IsNullOrEmpty(u.name))
+            {
+                storeLoginSessionId.name = u.username;
+            }
+            else
+            {
+                storeLoginSessionId.name = u.name;
+            }
+            textMesh.text = "Hello " + storeLoginSessionId.name;
+        }
+        else
+        {
+            Debug.Log("Login Failed");
+            textMesh.text = "";
+        }
+
+    }
+}
