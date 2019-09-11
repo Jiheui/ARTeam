489d2b8d1f5838920fbf67c8ab3c246f6830bfa1
Yue Jiang <u6311789@anu.edu.au>
2019-09-12 01:11:36 +1000

    A try on sharing poster

---
+++ ARPosterReader/Assets/DoNotDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoNotDestroy : MonoBehaviour
+{
+
+    public Text keyid;
+    void Awake()
+    {
+        
+
+        DontDestroyOnLoad(keyid);
+    }
+}
