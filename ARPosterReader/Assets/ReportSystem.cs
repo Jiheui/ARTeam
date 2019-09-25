7074c6da0d5a91ddc9044379c7f602663cd49173
ZEMIN/CHEN <u6293777@anu.edu.au>
2019-09-25 18:59:43 +1000

    Update Report
    Update Report, waiting for chirs's database

---
+++ ARPosterReader/Assets/ReportSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReportSystem : MonoBehaviour
+{
+    public Toggle isViolence;
+    public Toggle isEroticism;
+    public Toggle isFraud;
+
+    // record which toggle is on, 1 is violence, 2 is eroticisim, 3 is fraud
+    public int whichIsOn;
+    // the text report
+    public InputField report;
+
+    // check activity toggle
+    public int ActivityToggle()
+    {
+        int i = 0;
+        if (isViolence.isOn)
+        {
+            i = 1;
+            Debug.Log("isViolence is on"); 
+        }
+        else if (isEroticism.isOn)
+        {
+            i = 2;
+            Debug.Log("isEroticism is on");
+        }
+        else if (isFraud.isOn)
+        {
+            i = 3;
+            Debug.Log("isEroticism is on");
+        }
+        return i;
+    }
+
+    public void onSubmit()
+    {
+        
+        whichIsOn = ActivityToggle();
+        // did not click any toggle
+        if (whichIsOn == 0)
+        {
+            Debug.Log("which is on:   " + whichIsOn);
+            Debug.Log("Report text is:   "+ report.text);
+        }
+        else
+        {
+            // DataBase operation
+            Debug.Log("which is on:   " + whichIsOn);
+            Debug.Log("Report text is:   " + report.text);
+
+
+        }
+
+
+    }
+
+}
