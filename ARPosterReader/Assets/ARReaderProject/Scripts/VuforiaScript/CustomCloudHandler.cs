036e1aa2eb412ec3d214b28e0b07c1d4cf6172a3
ZEMIN/CHEN <u6293777@anu.edu.au>
2019-09-30 18:40:45 +1000

    n-th Bugs fixing trial
    n-th Bug fixing trial

--- ARPosterReader/Assets/ARReaderProject/Scripts/VuforiaScript/CustomCloudHandler.cs
+++ ARPosterReader/Assets/ARReaderProject/Scripts/VuforiaScript/CustomCloudHandler.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using Vuforia;
 using UnityEngine.Networking;
 using Models;
+using UnityEngine.UI;
 
 public class CustomCloudHandler : MonoBehaviour, IObjectRecoEventHandler
 {
@@ -17,6 +18,16 @@ public class CustomCloudHandler : MonoBehaviour, IObjectRecoEventHandler
     public GameObject GraphTemplate;
 
     public string poster_id;
+
+    // the report option part
+    public Toggle isViolence;
+    public Toggle isEroticism;
+    public Toggle isFraud;
+
+    // record which toggle is on, 1 is violence, 2 is eroticisim, 3 is fraud
+    public int whichIsOn;
+    // the text report
+    public InputField report;
 
     public void OnInitialized(TargetFinder targetFinder)
     {
@@ -156,5 +167,62 @@ public class CustomCloudHandler : MonoBehaviour, IObjectRecoEventHandler
     {
 
     }
+
+    // check activity toggle
+    public int ActivityToggle()
+    {
+        int i = 0;
+        if (isViolence.isOn)
+        {
+            i = 1;
+            Debug.Log("isViolence is on");
+        }
+        else if (isEroticism.isOn)
+        {
+            i = 2;
+            Debug.Log("isEroticism is on");
+        }
+        else if (isFraud.isOn)
+        {
+            i = 3;
+            Debug.Log("isEroticism is on");
+        }
+        return i;
+    }
+
+    public void onSubmit()
+    {
+
+        whichIsOn = ActivityToggle();
+        Report r = new Report();
+        r.targetid = poster_id;
+        r.userid = storeLoginSessionId.loginId;
+        r.detail = report.text;
+
+
+
+
+        // did not click any toggle
+        if (whichIsOn == 0)
+        {
+            r.type = whichIsOn;
+            Debug.Log("which is on:   " + whichIsOn);
+            Debug.Log("Report text is:   " + report.text);
+        }
+        else
+        {
+            r.type = whichIsOn;
+            // DataBase operation
+            Debug.Log("which is on:   " + whichIsOn);
+            Debug.Log("Report text is:   " + report.text);
+
+
+        }
+        r.SendReport();
+
+
+    }
+
+
 
 }
